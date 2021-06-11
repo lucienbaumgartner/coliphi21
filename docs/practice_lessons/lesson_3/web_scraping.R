1393df3199681d4a2ca176d38f4b3bd2b3430757
lucienbaumgartner <luciengeorges.baumgartner@uzh.ch>
2021-06-11 13:43:42 +0200

    build v12

---
+++ docs/practice_lessons/lesson_3/web_scraping.R
@@ -0,0 +1,54 @@
+library(rvest)
+library(tidyverse)
+library(quanteda)
+library(readtext)
+library(httr)
+library(pdftools)
+
+rm(list=ls())
+setwd('~/Desktop/')
+
+## get a user agent here: https://www.whatismybrowser.com/guides/the-latest-user-agent/chrome
+useragent <- "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36"
+## override default user agent
+httr::user_agent(useragent)
+
+## downloading pdfs from google scholar
+#######################################
+## URL from which you want the pdfs
+URL <- "https://scholar.google.com/scholar?hl=en&q=normative+generics"
+## parse HTML
+page <- read_html(URL)
+## extract a-elements with 'pdf' in their link
+pdf_nodes <- html_nodes(page, xpath='//a[contains(@href, "pdf")]')
+pdf_nodes
+## mmh but we have more pdfs available..
+## we will have to be less specific, because some links apparently don't contain the word 'pdf'
+pdf_nodes_2 <- html_nodes(page, xpath='//a[@data-clk and @data-clk-atid and not(@id)]')
+pdf_nodes_2
+## better!!
+## extract actual links
+links <- html_attr(pdf_nodes_2, 'href') 
+links
+## download a file
+?download.file
+download.file(links[1], destfile = 'testfile.pdf')
+## make corpus
+txt <- readtext('testfile.pdf')
+txt
+df <- corpus(txt)
+df
+## add metadata
+pdftxt <- pdf_text('testfile.pdf')
+as_tibble(pdftxt)
+pdfmeta <- pdf_info('testfile.pdf')
+pdfmeta
+metadata <- tibble(title = pdfmeta$keys$Title, author = pdfmeta$keys$Author, 
+                   created = pdfmeta$created, modified = pdfmeta$modified)
+metadata
+## corpus has no metadata
+docvars(df)
+## add metadata
+docvars(df) <- metadata
+df
+docvars(df)
---
+++ practice_lessons/lesson_3/reddit_API.R
@@ -0,0 +1,127 @@
+library(tidyverse)
+library(jsonlite)
+library(stringi)
+library(quanteda)
+
+rm(list=ls())
+
+## simple comment-query on reddit
+#################################
+## formulate query
+search.term <- 'phenomonology'
+query <- paste0('https://api.pushshift.io/reddit/search/comment/?q=', search.term)
+query
+response <- fromJSON(query, flatten = TRUE)
+?str
+str(response, max.level = 2)
+## extract data
+df <- response$data
+View(df)
+## have a look at the texts
+df$body
+
+## AND: phenomenology AND Hegel
+################################
+search.term <- 'phenomonology+hegel'
+query <- paste0('https://api.pushshift.io/reddit/search/comment/?q=', search.term)
+query
+response <- fromJSON(query, flatten = TRUE)
+df <- response$data
+table(grepl('(hegel.*phenomonology)|(phenomonology.*hegel)', tolower(df$body)))
+
+## OR: phenomenology OR logicism
+################################
+search.term <- 'phenomonology|logicism'
+query <- paste0('https://api.pushshift.io/reddit/search/comment/?q=', search.term)
+query
+response <- fromJSON(query, flatten = TRUE)
+df <- response$data
+table(grepl('phenomonology|logicism', tolower(df$body)))
+
+## Phrases: "constructive criticism"
+###################################
+search.term <- '%22constructive%20criticism%22'
+query <- paste0('https://api.pushshift.io/reddit/search/comment/?q=', search.term)
+query
+response <- fromJSON(query, flatten = TRUE)
+df <- response$data
+df
+
+## all possible parameters here: https://reddit-api.readthedocs.io/en/latest/#using-the-https-api-pushshift-io-endpoints
+
+
+#######################################
+## From the query to a corpus object ##
+#######################################
+search.terms <- c('"awful holiday"', 'basejumping', 'supervenience')
+search.terms
+search.terms.coll <- paste0(search.terms, collapse = '|')
+search.terms.coll
+## replace space with its URL-encoding
+query_tail <- gsub('\\s', '%20', search.terms.coll)
+query_tail
+## replace quote with URL-encoding
+query_tail <- gsub('\\"', '%22', query_tail)
+query_tail
+## query
+query <- paste0('https://api.pushshift.io/reddit/search/comment/?q=', query_tail)
+response <- fromJSON(query, flatten = TRUE)
+## response is a list containing vectors and lists_
+str(response, max.level = 2)
+## so:
+## the response is a list and must be unpacked
+## some of the columns in $data are list themselves and must be dealt with
+response <- response$data # unpack $data
+sapply(response, is.list) # check which columns are lists
+## remove list elements
+listless <- response[!sapply(response, is.list)]
+listless <- as_tibble(listless)
+listless
+## rename 
+df <- listless
+## check out the text data
+head(df$body)
+## annotate which of your search term actually occurs in each response
+## coerce text data to lower case
+df$body <- tolower(df$body)
+## create regex
+search.terms.coll
+search.regex <- gsub('\\"', '', search.terms.coll) # remove quotes that were needed for API query
+search.regex
+## extract matches
+regmatch <- stri_extract_all(df$body, regex = search.regex)
+regmatch # you might also have NAs
+## inspect them
+df$body[sapply(regmatch, is.na)] # I think this one we can safely drop
+## now we join the match-annotation to the data
+## check that the match-vector is same length as the number of rows in the data
+if(nrow(df) == length(unlist(regmatch))){
+  ## IF TRUE:
+  df$rmatch <- unlist(regmatch)
+}else{
+  ## IF FALSE:
+  #regmatch[[1]] <- c(regmatch[[1]], 'falsepositive')
+  #nrow(df) == length(unlist(regmatch)) # now this turns out false
+  head(regmatch)
+  lengths(regmatch)
+  # data inflation
+  df <- df[rep(1:nrow(df), times = lengths(regmatch)), ]
+  df$rmatch <- unlist(regmatch)
+}
+## filter out data without matches
+#df <- filter(df, !rmatch == 'falsepositive')
+df <- filter(df, !is.na(rmatch))
+
+### TADAAAAA now you have your data
+## check how many times each search word occurs
+table(df$rmatch)
+## create quanteda-corpus
+corp <- corpus(df, text_field = 'body', docid_field = 'id')
+corp
+## subset documents for supervenience
+docvars(corp)
+sup <- corpus_subset(corp, rmatch == 'supervenience')
+sup
+
+
+# https://api.pushshift.io/reddit/comment/search/?q=universe&after=1620848893&before=24h
